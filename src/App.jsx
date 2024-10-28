209e71c7f8212600251c13a5dc4481decb91a351
krishnarrana <krishna2121.rana@gmail.com>
2024-10-28 13:15:33 +0100

    User Input Component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
